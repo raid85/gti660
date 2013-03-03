a542b108b8db7b3d10edce34f01655c8f48338d6
chebli.r@gmail.com <chebli.r@gmail.com@b63a7ef3-644c-59e4-195a-69765967b4e2>
2013-03-03 20:47:59 +0000

    Connected to bd throu tomcat and added jdbc librairies

---
+++ trunk/GTI660WEB/src/dbh/RSQObject.java
@@ -0,0 +1,15 @@
+package dbh;
+
+
+/**
+* Generic container for object retreived from the database  
+*
+*/
+ 
+public class RSQObject extends Object
+{
+	public RSQObject()
+	{
+		super(); 
+	}
+}
--- trunk/GTI660WEB/src/modele/DBConnection.java
+++ trunk/GTI660WEB/src/modele/DBConnection.java
@@ -1,11 +1,50 @@
 package modele;
 
+import dbh.*;
+
 public class DBConnection {
 		
+	public DBConnection() {
+		connectionBD();
+	}
+	private boolean isUp = false; 
 	private String sid = "GTI660" ;
 	private String user = "equipe4" ;
 	private String password ="8M2Fbjt6" ;
 	private String Serveur = "oracle-11.logti.etsmtl.ca";
+	private String port = "1521" ;
+	private RSQConnect connect = new RSQConnect();
+	
+	
+	
+	 private void connectionBD() {
+			String user = this.user ; 
+			String passwd = this.password ; 
+			String host = this.Serveur;   
+			String port = this.port;    
+			String dbname = this.getSid();  
+			try {			
+				connect.open(new RSQUser(user, passwd), new RSQAddress(host, port, dbname));}
+			 catch (Exception e) {
+					System.err.println("(RSQConnect) Cannot connect to DB because : " + e.getMessage());
+					
+					}
+	   }
+	
+	
+	
+	public String getPort() {
+		return port;
+	}
+
+
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+
+
 	public String getSid() {
 		return sid;
 	}
@@ -37,6 +76,12 @@ public class DBConnection {
 		infosC[3]="Password : "+this.password;
 		infosC[1]="Session ID (SID) : "+this.sid;
 		return infosC;
+	}
+
+
+
+	public boolean isUp() {
+		return connect.isOpen();
 	}
 
 }
